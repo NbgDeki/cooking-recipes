ff2d98a2ce9e54d39f9b2e4851aed47e19464541
dr250385 <dr250385@WIN-10M20X2>
2024-08-15 10:06:21 +0200

    Making a search bar component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -16,7 +16,9 @@ export default function Home() {
   return (
     <div className='home'>
       {error && <p className='error'>{error}</p>}
+
       {isPending && <p className='loading'>Loading...</p>}
+
       {recipes && <RecipeList recipes={recipes}></RecipeList>}
     </div>
   );
